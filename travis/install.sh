47b93bebfd34bfe0feb0b39f1d936c3a0c766131
Eric GELOEN <geloen.eric@gmail.com>
2017-02-09 00:12:06 +0100

    [Docker] Add HHVM container

---
+++ travis/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+COMPOSER_PREFER_LOWEST=${COMPOSER_PREFER_LOWEST-false}
+DOCKER_BUILD=${DOCKER_BUILD-false}
+SYMFONY_VERSION=${SYMFONY_VERSION-2.7.*}
+
+if [ "$DOCKER_BUILD" = true ]; then
+    cp .env.dist .env
+
+    docker-compose build
+    docker-compose run --rm php composer update --prefer-source
+
+    exit
+fi
+
+composer self-update
+
+composer require --no-update symfony/framework-bundle:${SYMFONY_VERSION}
+
+if [[ "$SYMFONY_VERSION" =~ ^3\.[1-9][0-9]? ]]; then
+    composer require --no-update --dev symfony/cache:${SYMFONY_VERSION}
+fi
+
+composer remove --no-update --dev friendsofphp/php-cs-fixer
+
+if [[ "$SYMFONY_VERSION" = *dev* ]]; then
+    composer config minimum-stability dev
+fi
+
+composer update --prefer-source `if [ "$COMPOSER_PREFER_LOWEST" = true ]; then echo "--prefer-lowest --prefer-stable"; fi`
